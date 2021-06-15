f8e54091be1357a7f877f9389bf504a339121c83
w3aewander <wander.silva@gmail.com>
2021-06-15 16:00:28 -0300

    Criação de RestAPI para injetar localização via JavaScript

--- src/main/java/br/com/wmomodas/wmomodas/PositionController.java
+++ src/main/java/br/com/wmomodas/wmomodas/PositionController.java
@@ -5,6 +5,8 @@ import br.com.wmomodas.persistence.PositionRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping({"/position"})
 public class PositionController {
@@ -15,6 +17,21 @@ public class PositionController {
         this.repository = positionRepository;
     }
 
+    /**
+     * GET ALL:  Obtém a lista de registros.
+     * @return lista de registros
+     */
+    @GetMapping
+    public List findAll(){
+        return repository.findAll();
+    }
+
+    /**
+     * UPDATE: Atualizar o registro
+     * @param id
+     * @param position
+     * @return
+     */
     @PutMapping(value="/{id}")
     public ResponseEntity update(@PathVariable("id") long id,
                                  @RequestBody Position position) {
@@ -29,10 +46,42 @@ public class PositionController {
                 }).orElse(ResponseEntity.notFound().build());
     }
 
+    /**
+     * GET: Obter o registro informado
+     * @param id
+     * @return
+     */
     @GetMapping(path = {"/{id}"})
     public ResponseEntity findById(@PathVariable long id){
         return repository.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
     }
+
+
+    /**
+     * POST: Criar ou acrescentar novo registro
+     * @param position
+     * @return
+     */
+    @PostMapping
+    public Position create(@RequestBody Position position){
+        return repository.save(position);
+    }
+
+
+    /**
+     * DELETE - Excluir o registro
+     * @param id
+     * @return
+     */
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity <?> delete(@PathVariable long id) {
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
 }
